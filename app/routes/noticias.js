34fc3b495b061d6091f676b6632d9a8e062ea031
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-06-22 11:50:28 -0300

    Separação do módulo de BD

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,18 +1,16 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function (app) {
 
+    var connection = dbConnection();
+
     app.get('/noticias', function (req, res) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'admin',
-            database: 'portal_noticias'
-        });
 
         connection.query('select * from noticias', function (error, result) {
-            res.send(result);
+            res.render('noticias/noticias', {
+                noticias: result
+            });
         })
 
-        // res.render('noticias/noticias');
     });
 }
